95c7e3a8fad1b55dabb6c194b6a3de254963a9d5
MarkusJoe <markushammered@gmail.com>
2022-09-18 09:18:43 +0800

    update: 更新配置文件

--- src/config.py
+++ src/config.py
@@ -15,10 +15,11 @@ class Config:
     database file path is "./src/db/data.sqlite", it is not exists, before you run server first
     remote sqlite file url is "https://pac.rtst.tech/static_file_hosting/static/counter/data.sqlite"
     you can download it manually
-    if you want to use mysql then replace "mysql|[Username]:[password]@[host]:[port]/[database]" with "sqlite"
-    example: mysql-root:114514@127.0.0.1:3306/homo
+    if you want to use mysql then replace "mysql://[Username]:[password]@[host]:[port]/[database]" with "sqlite"
+    example: mysql://root:114514@127.0.0.1:3306/homo
     """
-    database = "sqlite"
+    database = "mysql://counter:114514@111.173.80.154:26098/Counter"
+    # database = 'sqlite'
     if 'sqlite' in database:
         os.environ['counter_db_type'] = 'sqlite'
     elif 'mysql' in database:
