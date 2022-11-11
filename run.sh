ca1e31e86699053cd0543ddcb0aeb129fc45658a
MarkusJoe <markushammered@gmail.com>
2022-11-11 18:46:32 +0800

    update: 更新文档细节

--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-uvicorn src:create_app --factory --host=0.0.0.0
--- src/scripts/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -- coding:utf-8 --
-# @Author: markushammered@gmail.com
-# @Development Tool: PyCharm
-# @Create Time: 2022/10/2
-# @File Name: __init__.py
