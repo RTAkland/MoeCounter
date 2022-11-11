496305bc58e387eace691e7f509719f3ca3bd54c
MarkusJoe <markushammered@gmail.com>
2022-11-11 18:48:02 +0800

    update: 统一了除/export/api返回的数据风格更新为RESTFul风格

--- src/api/view.py
+++ src/api/view.py
@@ -5,25 +5,46 @@
 # @Create Time: 2022/9/2
 # @File Name: view.py
 
+import time
 
 from src.api import api
 from src.db import Database
 from fastapi.responses import FileResponse
+from fastapi.responses import JSONResponse
+
+
+async def _time():
+    return time.time()
 
 
 @api.get('/query/{name}')
 async def query(name: str):
-    times = Database().query(name)[1]
-    return {name: times}
+    data = Database().query(name)
+    response = {
+        'code': 200,
+        'time': await _time(),
+        'data': {
+            'name': data[0],
+            'times': data[1]
+        }
+    }
+    return response
 
 
 @api.get('/query-all/')
 async def query_all(limit: int = 30):
     data = Database().query_all()[:limit]
-    result = {}
+    response = {
+        'code': 200,
+        'time': await _time(),
+        'data': []
+    }
     for i in data:
-        result[i[0]] = i[1]
-    return result
+        response['data'].append({
+            'name': i[0],
+            'times': i[1]
+        })
+    return response
 
 
 @api.get('/export/')
@@ -34,4 +55,16 @@ async def export():
 @api.get('/query-theme/{name}')
 async def query_theme(name: str):
     data = Database().query_image(name)
-    return data
+    response = {
+        'code': 200,
+        'time': await _time(),
+        'data': []
+    }
+    for i in data:
+        response['data'].append({
+            'index': i[0],
+            'image': i[1],
+            'width': i[2],
+            'height': i[3]
+        })
+    return JSONResponse(response)
